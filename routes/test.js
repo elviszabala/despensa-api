b440f1c42ccea2243f539d41630149539431eaf2
Elvis <zabalaelvis@hotmail.com>
2025-08-18 22:27:07 +0200

    Test
    Change on test route

--- routes/test.js
+++ routes/test.js
@@ -3,7 +3,26 @@ const router = express.Router();
 const sql = require('../db/neonClient');
 const verificarToken = require('../middlewares/verificarToken');
 
-router.get('/', verificarToken, async (req, res) => {
+router.get('/', async (req, res) => {
+  console.log('Petición recibida en la ruta /test');
+  try {
+    console.log('Conectando a la base de datos...');
+    const result = await new Promise ((resolve) => setTimeout(resolve("Test"), 1000))
+
+    // Si quieres mostrar todos los usuarios:
+    res.status(200).json(result);
+
+    // Si solo quieres mostrar la versión (como en tu ejemplo):
+    // const { version } = result[0];
+    // res.writeHead(200, { "Content-Type": "text/plain" });
+    // res.end(version);
+  } catch (error) {
+    console.error('Error al consultar la base de datos:', error);
+    res.status(500).json({ error: 'Error al consultar la base de datos' });
+  }
+});
+
+/* router.get('/', verificarToken, async (req, res) => {
   console.log('Petición recibida en la ruta /test');
   try {
     console.log('Conectando a la base de datos...');
@@ -20,6 +39,6 @@ router.get('/', verificarToken, async (req, res) => {
     console.error('Error al consultar la base de datos:', error);
     res.status(500).json({ error: 'Error al consultar la base de datos' });
   }
-});
+}); */
 
 module.exports = router;
