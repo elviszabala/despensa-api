3b44a0e750cc6222a6d0ac7ae8d8cd94fc7afa44
Elvis <zabalaelvis@hotmail.com>
2025-08-18 23:05:33 +0200

    Test Route
    Post route add

--- routes/test.js
+++ routes/test.js
@@ -27,6 +27,30 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.post('/', async (req, res) => {
+  console.log('Petición recibida en la ruta /test');
+   if (!req.body || !req.body.email) {
+        return res.status(400).json({ mensaje: 'Email y contraseña son requeridos' });
+   // if (!errores.isEmpty()) return res.status(400).json({ errores: errores.array() });
+    
+  }
+  try {
+    console.log('Conectando a la base de datos...');
+    const result = await new Promise ((resolve) => setTimeout(resolve("Test: " + req.body.email), 1000))
+
+    // Si quieres mostrar todos los usuarios:
+    res.status(200).json(result);
+
+    // Si solo quieres mostrar la versión (como en tu ejemplo):
+    // const { version } = result[0];
+    // res.writeHead(200, { "Content-Type": "text/plain" });
+    // res.end(version);
+  } catch (error) {
+    console.error('Error al consultar la base de datos:', error);
+    res.status(500).json({ error: 'Error al consultar la base de datos' });
+  }
+});
+
 /* router.get('/', verificarToken, async (req, res) => {
   console.log('Petición recibida en la ruta /test');
   try {
